c00f74bfe6fa4dfd6a5d35c853f84ba70030ca29
bajpaisushil <sushilbajpai2003@gmail.com>
2023-12-09 10:53:38 +0530

    db

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
     try {
         console.log(process.env.MONGO_URI)
-        const conn = await mongoose.connect(process.env.MONGO_URI, {});
+        const conn = await mongoose.connect(process.env.MONGO_URI);
         console.log(`Mongodb connected with server: ${conn.connection.host}`);
     } catch (error) {
         console.error(`MongoDB Error: ${error.message}`);
